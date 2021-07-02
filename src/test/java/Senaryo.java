6f24ff29bc2cf20988cede2ff6fd8c388943eb77
hasancanisbeceren <dainstell@outlook.com>
2021-07-02 14:08:35 +0300

    Initial commit

---
+++ src/test/java/Senaryo.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+public class Senaryo extends BaseTest {
+    Steps steps = new Steps();
+
+    @Test
+    public void TestSenaryosu() throws Exception {
+        steps.ifElementExistClick("#gender-popup-modal > div > div > div.modal-close");// kadın/erkek pop-up ı çıkarsa kapat
+        steps.clickElement("#account-navigation-container > div > div.account-nav-item.user-login-container > div.link.account-user"); //Giriş yap ikonuna tıkla
+        steps.waitSecond(3); // 3 saniye bekle
+        steps.sendKeysToElementTest("input[id='login-email']", "xmyle@outlook.com"); //email inputuna değeri yaz
+        steps.sendKeysToElementTest("input[id='login-password-input']", "123deneme123"); // parola inputuna değeri yaz
+        steps.clickElement("#login-register > div.lr-container > div.q-layout.login > form > button");
+        steps.waitSecond(2);// giriş yap butonuna tıkla
+        //steps.getTextControlTest("#account-navigation-container > div > div.account-nav-item.user-login-container > div.link.account-user > p", "Hesabım"); //giriş yapılmış mı kontrol et
+        steps.sendKeysToElementTest("#auto-complete-app > div > div > input", "bilgisayar"); // arama çubuğuna "bilgisayar" yaz
+        steps.clickElement("i[class='search-icon']"); //arama ikonuna bas
+        steps.clickElement("body");
+        steps.randomClick("div[class='p-card-img-wr']"); // rastgele bir ürün seç
+        steps.waitSecond(2);// 2 saniye bekle
+        //*************************************************//Yeni açılan pencerede otomasyona devam etmiyor..!!!!!!!!
+        steps.clickElement("div[class='pr-cn-in'] div[class='add-to-bs-tx']"); //sepete ekle butonuna bas
+        steps.clickElement("#account-navigation-container > div > div.account-nav-item.basket-preview"); //sepetim ikonuna bas
+        steps.saveText("#partial-basket > div > div.pb-merchant-group > div.pb-basket-item > div.pb-basket-item-actions > div.pb-basket-item-price > span"); //ürünün fiyat bilgisini kaydet
+        steps.waitSecond(2); // 2 saniye bekle
+        steps.changeControl(); // Sepetim sayfasında ürünlere göre div değişikliği olduğu için method yazıldı
+        steps.clickElement("#partial-basket > div > div.pb-merchant-group > div.pb-basket-item > div.pb-basket-item-actions > button > i"); //sil butonuna bas
+        steps.clickElement("#ngdialog1 > div.ngdialog-content > form > div > div.footer > div > div.left > button.btn-item.btn-remove");//sil butonuna bas
+        steps.getTextControlTest("div[class='col-lg-8 col-md-8 col-xs-8'] span", "Sepetinizde ürün bulunmamaktadır."); // sepet boş mu kontrol et
+    }
+}
---
+++ src/test/java/Steps.java
@@ -0,0 +1,79 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class Steps extends Methods {
+
+    public void clickElement(String key) {
+        WebElement element = findElement(key);
+        clickToElement(element);
+    }
+
+    public void ifElementExistClick(String key) {
+        waitSecond(2);
+        List<WebElement> elements = driver.findElements(By.cssSelector(key));
+        if (elements.size() > 0) {
+            clickToElement(elements.get(0));
+        }
+    }
+
+    public void randomClick(String key) {
+        List<WebElement> elements = findElements(key);
+        clickToElement(elements.get(randomInt()));
+    }
+
+    public void sendKeysToElementTest(String key, String text) {
+        WebElement element = findElement(key);
+        sendKeysToElement(element, text);
+    }
+
+    public void waitSecond(int second) {
+        waitByMilliSeconds(second * 1000);
+    }
+
+    public void getTextControlTest(String key, String text) {
+        WebElement element = findElement(key);
+        getTextControl(element, text);
+    }
+
+    public void getAttributeControlTest(String key, String text) {
+        WebElement element = findElement(key);
+        getAttributeControl(element, text);
+    }
+
+    public void textControlTest(String key) {
+        WebElement element = findElement(key);
+        textControl(element);
+    }
+
+    public void saveText(String key) {
+        WebElement element = findElement(key);
+        saveStaticString(element);
+
+    }
+
+    public void SelectElement(String key, String text) {
+        SelectMethod(key, text);
+    }
+
+    public void changeControl() {
+        if (driver.findElements(By.cssSelector("div[class='pb-basket-item-price']")).size() > 0) {
+            textControlTest("div[class='pb-basket-item-price']"); // ürün fiyatını kontrol et
+            clickElement("button[class='ty-numeric-counter-button']"); //ürün adet arttır
+            waitSecond(2); // 2 saniye bekle
+            getAttributeControlTest("input[class='counter-content']", "2"); //ürün adeti 2 mi kontrol et
+            clickElement("i[class='i-trash']"); //ürünü kaldır butonuna bas
+
+
+        } else {
+            textControlTest("div[class*='priceInfo']"); //ürün fiyatını kontrol et
+            SelectElement("select[class*='basketItemQuantity']", "2"); //ürün adetini 2 yap
+            waitSecond(2); //2 saniye bekle
+            getTextControlTest("select[class*='basketItemQuantity'] option[selected='selected']", "2"); //ürün adeti 2 mi kontrol et
+            clickElement("a[class='removeitem']"); //ürünü kaldır butonuna bas
+
+        }
+
+    }
+}
